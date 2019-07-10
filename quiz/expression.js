037e67de52618f766ed2172545a38c80a261f3fb
HaniAboalinain <36819883+HaniAboalinain@users.noreply.github.com>
2019-07-10 13:28:06 +0300

    Add files via upload

---
+++ quiz/expression.js
@@ -0,0 +1,13 @@
+/*
+ * Programming Quiz: First Expression 
+ *
+ * Write an expression that uses at least three, different, arithmetic operators
+ * to log the number 42 to the console.
+ */
+
+// this expression equals 4, change it to equal 42
+
+console.log(4*5+24-2);
+console.log(4**4/4-22);
+console.log(2+(80/2));
+console.log((4+80)/2);
---
+++ quiz/favorite-food.js
@@ -0,0 +1,9 @@
+/*
+ * Programming Quiz: Favorite Food 
+ * Create a string with the name of your favorite food. The first letter of the string should be capitalized.
+ */
+
+
+var favoriteFood="Chicken";
+
+console.log(favoriteFood);
